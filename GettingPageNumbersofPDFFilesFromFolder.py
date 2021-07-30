07deb9792140f82161cad41aec873bbeda518fa5
SomannaKuri <somannalkuri@gmail.com>
2021-07-30 12:46:45 +0530

    Deleted unused variables

--- GettingPageNumbersofPDFFilesFromFolder.py
+++ GettingPageNumbersofPDFFilesFromFolder.py
@@ -7,11 +7,12 @@ Created on Fri Jul 30 11:56:57 2021
 import glob # Its part of standard system library, not required to install it
 from PyPDF2 import PdfFileReader # install using pip/conda install PyPDF2
 totalPages = 0 # adding total number of pages from each file
-k=0
+# adding all files from given directory to a list
 files=list((glob.glob(r"C:\Users\somanna.kuri\Desktop\WebDevelopmentTraining\Samanvi\*.pdf")))
+#Looping through all files and adding page number to variable totalPages
 for i in files:
     pdf = PdfFileReader((open(i,"rb")))
     totalPages.append(pdf.getNumPages())
     totalPages +=pdf.getNumPages()
-print("Total number of pages from above directory :")
+print("Total number of pages from above directory :", totalPages)
 
